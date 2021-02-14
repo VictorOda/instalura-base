d7fe89d1b61aaeab188175d43f3b270adeff68b0
Victor Oda <victor.ra.oda@gmail.com>
2021-02-14 18:42:09 -0300

    Fix app propstyle eslint error

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-console */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
-import PropTypes from 'prop-types';
 import theme from '../src/theme';
 import GlobalStyle from '../src/theme/GlobalStyle';
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -23,8 +24,3 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
-
-App.propTypes = {
-  Component: PropTypes.node.isRequired,
-  pageProps: PropTypes.node.isRequired,
-};
