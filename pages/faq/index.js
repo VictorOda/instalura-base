cdf81b4df6f6f1afd8dded92d79dc74dc9550a95
Victor Oda <victor.ra.oda@gmail.com>
2021-03-09 22:32:41 -0300

    Final da segunda aula do modulo 03

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import FAQScreen from '../../src/components/screens/FAQScreen';
 
-export default function FAQPage() {
+// eslint-disable-next-line react/prop-types
+export default function FAQPage({ faqCategories }) {
   return (
-    <div>FAQ</div>
+    <FAQScreen faqCategories={faqCategories} />
   );
+}
+
+export async function getStaticProps() {
+  const faqCategories = await fetch('https://instalura-api.vercel.app/api/content/faq')
+    .then((respostaDoServer) => respostaDoServer.json())
+    .then((respostaConvertida) => respostaConvertida.data)
+    .then((resposta) => resposta);
+  return {
+    props: {
+      faqCategories,
+    },
+  };
 }
