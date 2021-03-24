90cfab507db25b8eb8f4524f680204726fec2f50
Victor Oda <victor.ra.oda@gmail.com>
2021-03-23 22:25:47 -0300

    Final da parte 01

---
+++ src/services/login/loginService.js
@@ -0,0 +1,36 @@
+async function HttpClient(url, { headers, body, ...options }) {
+  return fetch(url, {
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+    ...options,
+  })
+    .then((respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        return respostaDoServidor.json();
+      }
+
+      throw new Error('Falha em pegar os dados do servidor');
+    });
+}
+
+export const loginService = {
+  async login({ username, password }) {
+    return HttpClient('https://instalura-api.omariosouto.vercel.app/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then((respostaConvertida) => {
+        console.log(respostaConvertida);
+        return respostaConvertida;
+      });
+  },
+};
