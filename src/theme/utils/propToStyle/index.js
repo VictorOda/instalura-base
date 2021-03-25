69b1c8ec40c0dfe19ecaf9da567607049aeeeca0
Victor Oda <victor.ra.oda@gmail.com>
2021-03-24 22:26:07 -0300

    Final da aula 04 do módulo 04

---
+++ src/theme/utils/propToStyle/index.js
@@ -0,0 +1,24 @@
+import { breakpointsMedia } from '../breakpointsMedia';
+
+export function propToStyle(propName) {
+  return (props) => {
+    const propValue = props[propName]; // string or object
+    if (typeof propValue === 'string' || typeof propValue === 'number') {
+      return {
+        [propName]: props[propName],
+      };
+    }
+    if (typeof propValue === 'object') {
+      const breakpoints = {};
+
+      if (propValue.xs) breakpoints.xs = { [propName]: propValue.xs };
+      if (propValue.sm) breakpoints.sm = { [propName]: propValue.sm };
+      if (propValue.md) breakpoints.md = { [propName]: propValue.md };
+      if (propValue.lg) breakpoints.lg = { [propName]: propValue.lg };
+      if (propValue.xl) breakpoints.xl = { [propName]: propValue.xl };
+
+      return breakpointsMedia(breakpoints);
+    }
+    return {};
+  };
+}
