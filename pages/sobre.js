8a36d7fcc471cdb8a25fa94e00afd08b5f3757e6
Victor Oda <victor.ra.oda@gmail.com>
2021-04-06 22:10:52 -0300

    Final da aula 02

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import { GraphQLClient, gql } from 'graphql-request';
+import AboutScreen from '../src/components/screens/AboutScreen';
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
-export default function Sobre() {
-  return (
-    <div>Sobre</div>
-  );
+export async function getStaticProps() {
+  const TOKEN = 'f3b10acbcde7199b218bec4518b074';
+  const DatoCMSURL = 'https://graphql.datocms.com/';
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  const query = gql`
+    query {
+      pageSobre {
+        pageTitle
+        pageDescription
+      }
+    }
+  `;
+
+  const messages = await client.request(query);
+
+  return {
+    props: {
+      messages,
+    },
+  };
 }
+
+export default websitePageHOC(AboutScreen, {
+  pageWrapperProps: {
+    seoProps: { headTitle: 'Sobre' },
+  },
+});
