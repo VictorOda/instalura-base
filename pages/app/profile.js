2fa649d2568afeb30be1b0010850ae4105baea07
Victor Oda <victor.ra.oda@gmail.com>
2021-04-11 20:23:31 -0300

    Fix post error

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import websiteProfilePageHOC from '../../src/components/wrappers/WebsiteProfilePage/hoc';
 import { authService } from '../../src/services/auth/authService';
@@ -9,20 +12,31 @@ const Post = styled.img`
   height: 250px;
 `;
 
-function ProfilePage(props) {
+function ProfilePage({ user, posts }) {
   return (
     <div>
       Página de Profile!
       <pre>
-        {JSON.stringify(props, null, 4)}
+        {JSON.stringify(user, null, 4)}
+        {JSON.stringify(posts, null, 4)}
       </pre>
       <div>
-        {props.posts.map((post) => (<Post src={post.photoUrl} key={post._id} />))}
+        {posts.map((post) => (<Post src={post.photoUrl} key={post._id} alt={post.description} />))}
       </div>
       <img src="https://media.giphy.com/media/bn0zlGb4LOyo8/giphy.gif" alt="Nicolas Cage" />
     </div>
   );
 }
+
+ProfilePage.propTypes = {
+  user: PropTypes.object,
+  posts: PropTypes.array,
+};
+
+ProfilePage.defaultProps = {
+  user: {},
+  posts: [],
+};
 
 export default websiteProfilePageHOC(ProfilePage, {
   pageWrapperProps: {
