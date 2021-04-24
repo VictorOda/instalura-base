58fef9296b9a3a4df346ce38c60fc8c8e535d898
Victor Oda <victor.ra.oda@gmail.com>
2021-04-24 11:06:33 -0300

    Remove comments

--- src/components/screens/app/ProfileMenu/PostCreation.pageObject.js
+++ src/components/screens/app/ProfileMenu/PostCreation.pageObject.js
@@ -35,20 +35,4 @@ export default class PostCretionObject {
 
     return this;
   }
-
-  // fillFormsFields({ user, password }) {
-  //   // preencher o input usuario
-  //   this.cy.get('#formCadastro input[name="usuario"]').type(user);
-  //   // preencher o input senha
-  //   this.cy.get('#formCadastro input[name="senha"]').type(password);
-
-  //   return this;
-  // }
-
-  // clickOnSubmit() {
-  //   // clicar no botão de submit
-  //   this.cy.get('#formCadastro button[type="submit"]').click();
-
-  //   return this;
-  // }
 }
