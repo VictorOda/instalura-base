ca216c7ca1acebfda7dfc92d30411215a9061783
Victor Oda <victor.ra.oda@gmail.com>
2021-04-25 11:58:22 -0300

    Update prod link

--- src/services/auth/authService.js
+++ src/services/auth/authService.js
@@ -8,7 +8,7 @@ const BASE_URL = isStagingEnv
   // Back End de DEV
   ? 'https://instalura-api-git-master.omariosouto.vercel.app'
   // Back End de PROD
-  : 'https://instalura-api.omariosouto.vercel.app';
+  : 'https://instalura-api-omariosouto.vercel.app';
 
 export const authService = (ctx) => {
   const cookies = parseCookies(ctx);
